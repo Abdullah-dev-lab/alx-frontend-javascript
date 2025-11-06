de1026f11e7f1fe3e5e173b10cd177bcb9f3e3cd
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-11-06 23:37:19 +0100

    make changes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,8 +2,9 @@ interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     readonly fullTimeEmployee: boolean;
-    readonly yearsOfExperience: number;
+    readonly yearsOfExperience?: number;
     readonly location: string;
+    readonly contract?: boolean;
 };
 
 const teacher1: Teacher = {
@@ -11,5 +12,6 @@ const teacher1: Teacher = {
     lastName: "Doe",
     fullTimeEmployee: true,
     yearsOfExperience: 8,
-    location: "New york"
+    location: "New york",
+    contract: false
 };
